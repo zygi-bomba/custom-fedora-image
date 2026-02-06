e302a47e9ffd3b83289a42cd2a3c098732df4eb7
zygi-bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-06 18:46:11 +0100

    dotfiles support

--- config/scripts/custom-setup.sh
+++ config/scripts/custom-setup.sh
@@ -3,14 +3,12 @@ set -euo pipefail
 
 echo "Konfigurowanie systemu i użytkownika..."
 
-# 1. Tworzymy użytkownika (jeśli chcesz mieć go od razu w obrazie)
 # Hasło jest puste - ustawisz je sobie po pierwszym zalogowaniu przez 'passwd'
 if ! id "uzytkownik" &>/dev/null; then
     useradd -m -G wheel -s /usr/bin/bash uzytkownik
     passwd -d uzytkownik
 fi
 
-# 2. Poprawiamy uprawnienia dla plików skopiowanych do /etc/skel
 chown -R root:root /etc/skel
 chmod -R 755 /etc/skel
 
