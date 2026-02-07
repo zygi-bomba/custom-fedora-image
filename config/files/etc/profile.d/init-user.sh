30f67a6d4302818f8b817959643fabf232554263
zygi bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-07 10:10:57 +0100

    Delete config/files/etc/profile.d/init-user.sh

--- config/files/etc/profile.d/init-user.sh
+++
@@ -1,9 +0,0 @@
-# /etc/profile.d/init-user.sh
-
-# Uruchom tylko jeśli:
-# 1. To jest sesja interaktywna
-# 2. Nie ma jeszcze pliku-znacznika
-if [[ $- == *i* ]] && [ ! -f "$HOME/.config/.home-initialized" ]; then
-    echo "Wykryto pierwsze logowanie. Konfiguruję środowisko..."
-    jg init_dots
-fi
