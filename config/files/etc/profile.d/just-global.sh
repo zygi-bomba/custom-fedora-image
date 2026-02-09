794093b0d1635f168afb8cc62e978b780d23a031
zygi bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-09 14:12:25 +0100

    Delete config/files/etc/profile.d directory

--- config/files/etc/profile.d/just-global.sh
+++
@@ -1,6 +0,0 @@
-# /etc/profile.d/just-global.sh
-
-# Alias 'jg' (jak Just Global) uruchamia systemowy justfile
-# --working-directory ~ jest ważne, aby stow i inne komendy 
-# domyślnie operowały na Twoim katalogu domowym, a nie na /etc
-alias jg='just --justfile /etc/justfile --working-directory ~'
