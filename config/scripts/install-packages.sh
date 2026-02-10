a906599eb7acb8996c06844cd712d0f64a3d1a9c
zygi bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-10 15:08:23 +0100

    Add Nix configuration and enable nix-daemon service

--- config/scripts/install-packages.sh
+++ config/scripts/install-packages.sh
@@ -6,4 +6,7 @@
 set -oue pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --init none --no-start-daemon --no-confirm && 
+echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf && \
+systemctl enable nix-daemon.service
+
 curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
