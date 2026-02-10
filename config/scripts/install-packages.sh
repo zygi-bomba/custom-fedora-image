2032dfcf05a07c44203cb997e20930fa471eb62e
zygi bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-10 15:15:59 +0100

    Add Nix and DevBox installation commands

--- config/scripts/install-packages.sh
+++ config/scripts/install-packages.sh
@@ -5,8 +5,9 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+# Nix Install
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --init none --no-start-daemon --no-confirm && 
 echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf && \
-systemctl enable nix-daemon.service
 
+# DevBox install
 curl -fsSL https://get.jetify.com/devbox | bash -s -- -f
