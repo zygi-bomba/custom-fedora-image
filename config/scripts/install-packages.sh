3499e773c42a05db3701201bc7f424d51c287831
zygi bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-11 10:33:47 +0100

    Remove devpod installation from script
    Remove installation commands for devpod.

--- config/scripts/install-packages.sh
+++ config/scripts/install-packages.sh
@@ -5,5 +5,3 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-curl -L -o /usr/bin/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
-chmod +x /usr/bin/devpod
