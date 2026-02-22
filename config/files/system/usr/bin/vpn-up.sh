d16035f9caa491d1f16b22d4bb87db46cf34d049
zygi-bomba <wiktor.godula.stud@pw.edu.pl>
2026-02-22 20:25:03 +0100

    added virt manage flatpak

--- config/files/system/usr/bin/vpn-up.sh
+++ config/files/system/usr/bin/vpn-up.sh
@@ -3,7 +3,6 @@
 export VAULT_ADDR="http://192.168.2.100:8200"
 export VAULT_TOKEN="root-token"
 
-# 1. Pobieranie danych z Vault (używając jq dla bezpieczeństwa KV v2)
 HOME_JSON=$(vault kv get -format=json secrets/vpn/home)
 PW_JSON=$(vault kv get -format=json secrets/vpn/pw)
 
@@ -15,20 +14,13 @@ HOME_PUB=$(echo "$HOME_JSON" | jq -r '.data.data.public_key')
 ISOD_USER=$(echo "$PW_JSON" | jq -r '.data.data.username')
 ISOD_PASS=$(echo "$PW_JSON" | jq -r '.data.data.password')
 
-# 2. Iniekcja WireGuard (Home)
-# Twoja wersja nmcli nie pozwala na 'modify wireguard.peers'.
-# Rozwiązaniem jest bezpośrednia edycja parametrów sekcji peera.
 nmcli connection modify home \
   wireguard.private-key "$HOME_PRIV" \
   wireguard-peer."$HOME_PUB".endpoint "$HOME_ADDR" \
   wireguard-peer."$HOME_PUB".preshared-key "$HOME_PSK"
 
-# 3. Iniekcja AnyConnect (Split PW)
-# W Twoim pliku PW.nmconnection oba pola miały nazwę username.
-# Musimy to rozdzielić na username i password.
 nmcli connection modify "Split PW" \
   vpn.secrets "form:main:username=$ISOD_USER, form:main:password=$ISOD_PASS"
 
-# 4. Uruchomienie połączeń
 nmcli connection up home
 nmcli connection up "Split PW"
